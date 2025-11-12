13fa02018d791b89d0c0cdd06fa868557a1f5db1
Helenia <hm4945@princeton.edu>
2025-11-12 16:01:21 +0300

    Fix function signature for Node_new

--- 3FT/nodeFT.h
+++ 3FT/nodeFT.h
@@ -27,8 +27,8 @@ typedef struct node *Node_T;
                  or oNParent is NULL but oPPath is not of depth 1
   * ALREADY_IN_TREE if oNParent already has a child with this path
 */
-int Node_new(Path_T oPPath, Node_T oNParent, Node_T *poNResult, void *pvContents, 
-             size_t ulLength, boolean bIsFile);
+int Node_new(Path_T oPPath, Node_T oNParent,, boolean bIsFile, void *pvContents, 
+             size_t ulLength, Node_T *poNResult);
 
 /*
   Destroys and frees all memory allocated for the subtree rooted at
