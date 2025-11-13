bdd414fcc3dec9b339bab0ef9b19f5c4f6483a95
Helenia <hm4945@princeton.edu>
2025-11-13 07:38:38 +0300

    Update checkerFT.c

--- 3FT/checkerFT.c
+++ 3FT/checkerFT.c
@@ -75,7 +75,7 @@ static boolean checkerFT_Child_isValid(Node_T oNParent, Node_T oNChild,
         /* adding check for when directory comes before file*/
         if(!prevIsFile && currIsFile) {
           fprintf(stderr, "Children names out of order; the directory is before file: (%s) (%s)\n", 
-                  Path_getPathname(oNPrevChild), Path_getPathname(oNChild));
+                  Path_getPathname(Node_getPath(oNPrevChild)), Path_getPathname(Node_getPath(oNChild)));
           return FALSE;
           
         }
