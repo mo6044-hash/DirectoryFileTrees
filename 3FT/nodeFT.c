da7140a72b7def02e6d3517a243f84da3bd7f7a9
Helenia <hm4945@princeton.edu>
2025-11-14 00:31:11 +0300

    Refactor string comparison in nodeFT.c
    Replaced Path_compareString and Path_comparePath with strcmp for string comparison.

--- 3FT/nodeFT.c
+++ 3FT/nodeFT.c
@@ -56,7 +56,8 @@ static int Node_compareString(const Node_T oNFirst,
    assert(oNFirst != NULL);
    assert(pcSecond != NULL);
 
-   return Path_compareString(oNFirst->oPPath, pcSecond);
+   /* return Path_compareString(oNFirst->oPPath, pcSecond);*/
+   return strcmp(Path_getPathName(oNFirst->oPPath), pcSecond);
 }
 
 
@@ -332,7 +333,9 @@ int Node_compare(Node_T oNFirst, Node_T oNSecond) {
    assert(oNFirst != NULL);
    assert(oNSecond != NULL);
 
-   return Path_comparePath(oNFirst->oPPath, oNSecond->oPPath);
+   /* return Path_comparePath(oNFirst->oPPath, oNSecond->oPPath); */
+   return strcmp(Path_getNamePath(Node_getPath(oPPath)), 
+                 Path_getNamePath(Node_getPath(oPPath)));
 }
 
 char *Node_toString(Node_T oNNode) {
